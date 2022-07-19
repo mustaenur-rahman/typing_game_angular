fe3e0d37b41f8bf5fc51cc6e19a076656c161e0e
Md. Musta-E-Nur Rahman <md.musta-e-nur.rahman@b-architects.com>
2022-07-19 10:31:44 +0900

    Finished building typing game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'type';
+  randomSentence:string = faker.lorem.sentence();
+  inputtedText = '';
+  sentenceArray = this.randomSentence.split('');
+  inputtedTextArray: string[] = [];
+  onChangeText(value: string){
+    this.inputtedText = value;
+    this.inputtedTextArray = this.inputtedText.split(''); 
+  }
+
+  checkResult(){
+    if(this.inputtedText === this.randomSentence){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
 }
